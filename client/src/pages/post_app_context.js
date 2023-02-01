216b11d767d3a75b671051471f127eed7a2259a4
RJMesquita <rjmesquita@DESKTOP-7084MSP>
2023-02-01 18:01:02 +0000

    POST and PUT made with status code

--- client/src/pages/post_app_context.js
+++ client/src/pages/post_app_context.js
@@ -3,36 +3,34 @@ import {useShowOutputState, useOutputTextState, useDisableClickState } from '../
 import axios from 'axios';
 
 export const Post_app_context = () => {
+
+  const [responseData, setResponseData] = useState("");
+  const [outputText, setOutputText] = useOutputTextState();
+  const [showOutput, setShowOutput] = useShowOutputState();
+  //const [clicked, setClicked] = useDisableClickState();
+  const url = "http://127.0.0.1:8080/app_contexts"
+
   
-    const [outputText, setOutputText] = useOutputTextState();
-    const [showOutput, setShowOutput] = useShowOutputState();
-    //const [clicked, setClicked] = useDisableClickState();
-    const url = "http://127.0.0.1:8080/app_contexts"
+  const handleOutput = () => {
+    setShowOutput(!showOutput);
+  };
+  
+    // const handleClick = () => {
+    //   setClicked(true);
+    // };
 
-    
-    const handleOutput = () => {
-      setShowOutput(!showOutput);
-    };
-    
-      // const handleClick = () => {
-      //   setClicked(true);
-      // };
-
-    const handleSubmit = async event => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const data = JSON.parse(formData.get("body"));
-    
-    const outputText = await axios.post(url, data);
-    setOutputText(outputText.status);
-    /*
+  const handleSubmit = async event => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = JSON.parse(formData.get("body"));
     try {
       const outputText = await axios.post(url, data);
+      setResponseData(JSON.stringify(outputText.data));
       setOutputText(outputText.status);
     } catch (error) {
-      setOutputText(outputText.status);
-    }*/
-
+      setResponseData(JSON.stringify(error.response.data));
+      setOutputText(error.response.status)
+    }
   };
 
     //adicionar disabled={clicked} no button
@@ -43,7 +41,9 @@ export const Post_app_context = () => {
       <textarea type="text" name="body" placeholder="Insert the request body" />
       <div className="column">
       <button type="submit" onClick={handleOutput}>Submit</button>
-      {showOutput ? ( <p>{JSON.stringify(outputText, null, 10)}</p>) : null}
+      <p>Response status: {outputText}</p>
+      <h4>Body</h4>
+      <textarea value={responseData} readOnly />
       </div>
     </form>
   );
