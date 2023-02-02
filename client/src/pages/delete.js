0a911257505c7a8222e40fa64942916fac5f5f9a
RJMesquita <rjmesquita@DESKTOP-7084MSP>
2023-02-02 12:28:33 +0000

    post_loc, put and delete changed

--- client/src/pages/delete.js
+++ client/src/pages/delete.js
@@ -1,26 +1,47 @@
 import React, { useState } from 'react';
 import { useOutputTextState, useShowOutputState } from '../stateHooks';
+import axios from 'axios';
 
 export const Delete = () => {
-
+    const [responseData, setResponseData] = useState("");
     const [outputText, setOutputText] = useOutputTextState();
     const [showOutput, setShowOutput] = useShowOutputState();
+    const [input, setInput] = useState("");
+
+    const handleChange = event => {
+        setInput(event.target.value.trim());
+        };
 
     function handleOutput() {
         setShowOutput(!showOutput);
-      }
+        }
+
+        const handleDelete = async event => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const data = {contextId: formData.get("contextId")};
+
+    try {
+        const outputText = await axios.delete(`http://127.0.0.1:8080/app_contexts/${input}`, data);
+        setOutputText(outputText.status);
+    } catch (error) {
+        setResponseData(JSON.stringify(error.response.data));
+        setOutputText(error.response.status)    
+    }
+    };
 
     return (
-        <div>
-            <input className="url" placeholder="http://127.0.0.1:8080/app_contexts/{contextId}" type="text"/>            
-            <div className="parameters">
-                <div className="column">
-                    <input placeholder="contextId" type="text"/>
-                </div>
-            </div>
-            <button onClick={handleOutput}> Submit </button>
-            {showOutput ? (
-            <p>{outputText}</p>) : null}
-        </div>
-    )
-}
+        <form onSubmit={handleDelete}>
+          <p name="url">Sending to http://127.0.0.1:8080/app_contexts/{input}</p>  
+          <div className="column">
+            <input placeholder="contextId" name="contextId" type="text" value={input} onChange={handleChange}/>    
+          </div>
+          <div className="column">
+            <button type="submit" onClick={handleOutput}>Submit</button>
+            <p>Response status: {outputText}</p>
+            <h4>Body</h4>
+            <textarea value={responseData} readOnly />
+          </div>
+        </form>
+      );
+};
