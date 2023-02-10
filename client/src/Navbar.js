40159f3b27f889808799b5293acee0d932095dd4
Marina R Albuquerque <81524434+marinaralbu8298@users.noreply.github.com>
2023-02-10 17:42:56 +0000

    navbar text with the right color, footer started

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
   return (
     <nav>
       <div className='navbar'>
-      <img src={require('./images/logo_purple.png')} alt="Logo" className='logo' />
+        <Link to='/'>
+          <img
+            src={require('./images/logo_purple.png')}
+            alt="Logo"
+            className='logo'
+            style={{
+              backgroundColor: 'transparent',
+              border: 'none',
+              cursor: 'pointer'
+            }}
+          />
+        </Link>
         <div className='links'>
-          <Link className='get_link' to="/get"> Get App List </Link>
-          <Link className='post_link' to="/post_app_context"> Post App Context </Link>
-          <Link className='put_link' to="/put"> Put App Context </Link>
-          <Link className='delete_link' to="/delete"> Delete App Context </Link>
-          <Link className='post_link' to="/post_obtain_app_loc_availability"> Post Obtain App Location Availability </Link>
-          <Link className='notifications_link' to="/notifications"> Notifications </Link>
+          <Link className='get_link' to="/get"> GET APP LIST </Link>
+          <Link className='post_link' to="/post_app_context"> POST APP CONTEXT </Link>
+          <Link className='put_link' to="/put"> PUT APP CONTEXT </Link>
+          <Link className='delete_link' to="/delete"> DELETE APP CONTEXT </Link>
+          <Link className='post_link' to="/post_obtain_app_loc_availability"> POST OBTAIN APP LOCATION AVAILABILITY </Link>
+          <Link className='notifications_link' to="/notifications"> NOTIFICATIONS </Link>
         </div>
       </div>
     </nav>
