2de0d621e918e6ec53bba2d62bd516f4e47da9e2
Marina R Albuquerque <81524434+marinaralbu8298@users.noreply.github.com>
2023-02-15 18:30:08 +0000

    login button working

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -6,6 +6,7 @@ export const Navbar = () => {
 
   const [OutputText, setOutputText] = useState("");
   const url = "http://127.0.0.1:5005/login"; 
+
   function handleOutput() {
     axios.get(url)
       .then(response => {
@@ -15,6 +16,11 @@ export const Navbar = () => {
         setOutputText(error.response.status);
       });
   }
+
+  
+  const handleClick = () => {
+    window.location.href = 'http://127.0.0.1:5005/login';
+  };
 
   return (
     <nav>
@@ -39,7 +45,7 @@ export const Navbar = () => {
           <Link className='post_link' to="/post_obtain_app_loc_availability"> POST OBTAIN APP LOCATION AVAILABILITY </Link>
           <Link className='notifications_link' to="/notifications"> NOTIFICATIONS </Link>
         </div>
-        <button className="text-image-button" onClick={handleOutput}>
+        <button className="text-image-button" onClick={() => { handleClick(); handleOutput() }}>
           <img src="./images/google-logo.png" alt="GoogleLogo"/>
           <span>Login</span>
         </button>
