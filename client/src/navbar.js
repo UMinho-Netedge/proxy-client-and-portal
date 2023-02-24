6795c28e407d7313eafa57f17c717f4367927da5
Marina R Albuquerque <81524434+marinaralbu8298@users.noreply.github.com>
2023-02-24 15:28:52 +0000

    Cookie started

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -1,25 +1,42 @@
 import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useCookies } from 'react-cookie';
 
 export const Navbar = () => {
+  const [accessToken, setAccessToken] = useState("");
+  const [error, setError] = useState(null);
+  const [cookies, setCookie] = useCookies(['access_token']);
 
-  const [OutputText, setOutputText] = useState("");
-  const url = "http://127.0.0.1:5005/login"; 
-
-  function handleOutput() {
-    axios.get(url)
-      .then(response => {
-        setOutputText(response.status);
-      })
-      .catch(error => {
-        setOutputText(error.response.status);
-      });
-  }
-
-  const handleClick = () => {
+  const openLogin = () => {
     window.open('http://localhost:5000/login', '_blank', 'noreferrer');
   };
+  
+  const handleClick = () => {
+    try {
+      const response = fetch('http://localhost:5000/login')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      })
+      .catch(function () {
+        console.log('Booo');
+      });
+      console.log(response.json())
+      const data = response.data;
+      setAccessToken(data["access_token"]);
+      setCookie('access_token', data["access_token"], { path: '/' });
+      setError(null);
+    } catch (err) {
+      setAccessToken("");
+      setCookie('access_token', '', { path: '/' });
+      setError("Failed to get access token.");
+    }
+  };
+
+  
 
   return (
     <nav>
@@ -45,11 +62,11 @@ export const Navbar = () => {
           <Link className='notifications_link' to="/notifications"> NOTIFICATIONS </Link>
         </div>
         <div className="button-container">
-        <button className="text-image-button" onClick={() => { handleClick(); handleOutput() }}>
-          <img src={'./google-logo.png'} alt="GoogleLogo"/>
-          <span>Login</span>
-        </button>
-      </div>
+          <button className="text-image-button" onClick={() => { handleClick(); openLogin() }}>
+            <img src={'./google-logo.png'} alt="GoogleLogo"/>
+            <span>Login</span>
+          </button>
+        </div>
       </div>
     </nav>
   );
