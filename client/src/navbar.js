da950ee7aceeeb053d0d374e6d3ea660720143da
Marina R Albuquerque <81524434+marinaralbu8298@users.noreply.github.com>
2023-02-28 17:02:17 +0000

    login form added

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -8,37 +8,37 @@ export const Navbar = () => {
   const [error, setError] = useState(null);
   const [cookies, setCookie] = useCookies(['access_token']);
 
-  const openLogin = () => {
-    window.open('http://localhost:5000/login', '_blank', 'noreferrer');
-    // const headers = getAllResponseHeaders();
-    // console.log(headers)
-  };
+  //  const openLogin = () => {
+  //    window.open('http://localhost:5000/login', '_blank', 'noreferrer');
+  //    // const headers = getAllResponseHeaders();
+  //    // console.log(headers)
+  //  };
   
-  const handleClick = async () => {
-     try {
-      const response = await fetch('http://localhost:5000/login', {
-        redirect: 'follow',
-        mode: 'no-cors',
-        headers: {
-          'Access-Control-Allow-Origin':'*'
-        }});
-      const data = response.headers["Authorization"];
-      setAccessToken(data["access_token"]);
-      setCookie('access_token', data["access_token"], { path: '/' });
-      setError(null);
+  // const handleClick = async () => {
+  //    try {
+  //     const response = await fetch('http://localhost:5000/login', {
+  //       redirect: 'follow',
+  //       mode: 'no-cors',
+  //       headers: {
+  //         'Access-Control-Allow-Origin':'*'
+  //       }});
+  //     const data = response.headers["Authorization"];
+  //     setAccessToken(data["access_token"]);
+  //     setCookie('access_token', data["access_token"], { path: '/' });
+  //     setError(null);
         
-      console.log(response.headers);
+  //     console.log(response.headers);
         
-      } 
-      catch (error) {
-        setAccessToken("");
-        setCookie('access_token', '', { path: '/' });
-        setError("Failed to get access token.");
-        console.error('Error fetching data:', error);
-        console.log("Can't get access token")
-        return null;
-      }
-  }; 
+  //     } 
+  //     catch (error) {
+  //       setAccessToken("");
+  //       setCookie('access_token', '', { path: '/' });
+  //       setError("Failed to get access token.");
+  //       console.error('Error fetching data:', error);
+  //       console.log("Can't get access token")
+  //       return null;
+  //     }
+  // }; 
   
   return (
     <nav>
@@ -64,10 +64,12 @@ export const Navbar = () => {
           <Link className='notifications_link' to="/notifications"> NOTIFICATIONS </Link>
         </div>
         <div className="button-container">
-          <button className="text-image-button" onClick={() => { handleClick(); openLogin() }}>
-            <img src={'./google-logo.png'} alt="GoogleLogo"/>
-            <span>Login</span>
-          </button>
+          <Link to="/login">          
+            <button className="text-image-button">
+              <img src={'./google-logo.png'} alt="GoogleLogo"/>
+              <span>Login</span>
+            </button>
+          </Link>
         </div>
       </div>
     </nav>
