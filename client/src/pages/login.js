eb77bd578a7e588d8f8077c5e3c5187b53b83a21
Marina R Albuquerque <81524434+marinaralbu8298@users.noreply.github.com>
2023-03-01 12:10:19 +0000

    submit button working

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const url = 'http://127.0.0.1:5000/login';
 
-    return(
-    <form>
-        <label>
-            Username:
-            <input type="text" name="username" />
-        </label>
-    <br />
-        <label>
-            Password:
-            <input type="password" name="password" />
-        </label>
-    <br />
-        <button type="submit">Submit</button>
-</form>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(url, {
+      mode: 'no-cors',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin':'*'
+      },
+      body: JSON.stringify({ username, password })
+    })
+      .then(response => {
+        console.log("Success")
+      })
+      .catch(error => {
+        console.log("Error")
+      });
+  };
 
-    )
-
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default Login;
