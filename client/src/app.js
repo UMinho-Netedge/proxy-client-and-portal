0478b14d0b1b2090b045afc7a13529b7d5e0db8b
Marina R Albuquerque <81524434+marinaralbu8298@users.noreply.github.com>
2023-03-02 12:33:10 +0000

    partners css added

--- client/src/app.js
+++ client/src/app.js
@@ -11,87 +11,71 @@ import { Notifications } from './pages/notifications';
 import { Login }  from './pages/login';
 
 function Home() {
-
-
   if (window.location.pathname === '/') {
     return (
-        <div className='page'>
-          <div className='partners'>
-            <h4>Partners</h4>
-            <div className="column">
-              <div className='image-text'>
-                <img  src={require('./images/dstelecom.png')} alt="dstelecom" className="partner_imgs"/>
-                <h4>DSTELECOM S.A.</h4>
-                <h4>Leader Company</h4>
-              </div>
-              <div className='image-text'>
-                <img  src={require('./images/bee.png')} alt="bee" className="partner_imgs" />
-                <h4>Bee Engineering S.A.</h4>
-                <h4>Company</h4>
-              </div>
-              <div className='image-text'>
-                <img  src={require('./images/fapajal.png')} alt="fapajal" className="partner_imgs" />
-                <h4>FAPAJAL PAPERMAKING S.A.</h4>
-                <h4>Company</h4>
-              </div>
+      <div className='page'>
+        <div className='partners'>
+          <h4 className='partners_text'>Partners</h4>
+          <div className='image-text-row'>
+            <div className='image-text'>
+              <img src={require('./images/dstelecom.png')} alt="dstelecom" className="partner_imgs"/>
+              <h4 className='company_name'>DSTELECOM S.A.</h4>
+              <h4 className='company'>Leader Company</h4>
             </div>
-            <div className="column">
-              <div className='image-text'>
-                <img  src={require('./images/bySteel.png')} alt="bySteel" className="partner_imgs" />
-                <h4>BYSTEEL FS S.A</h4>
-                <h4>Company</h4>
-              </div>
-              <div className="column">
-                <img  src={require('./images/uMinho.png')} alt="uMinho" className="partner_imgs" />
-                <h4>University of Minho</h4>
-                <h4>Academia</h4>
-              </div>
-              <div className="column">
-                <img  src={require('./images/it.png')} alt="it" className="partner_imgs" />
-                <h4>Instituto de Telecomunicações</h4>
-                <h4>Academia</h4>
-              </div>
+            <div className='image-text'>
+              <img src={require('./images/bee.png')} alt="bee" className="partner_imgs" />
+              <h4 className='company_name'>Bee Engineering S.A.</h4>
+              <h4 className='company'>Company</h4>
+            </div>
+            <div className='image-text'>
+              <img src={require('./images/fapajal.png')} alt="fapajal" className="partner_imgs" />
+              <h4 className='company_name'>FAPAJAL PAPERMAKING S.A.</h4>
+              <h4 className='company'>Company</h4>
             </div>
           </div>
-
-          <div className='cofinanced'>
-            <h4>Co-financed by</h4>
-            <a href='https://www.compete2020.gov.pt/'>
-              <img
-                className='compete2020'
-                src={require('./images/level_compete.png')}
-                alt='compete2020'
-              />
-            </a>
-            <a href='https://portaldosincentivos.pt/index.php/portugal-2020'>
-              <img
-                className='portugal2020'
-                src={require('./images/level_portugal-2020.png')}
-                alt='portugal2020'
-              />
-            </a>
-            <a href='https://ec.europa.eu/regional_policy/funding/erdf_en?etrans=pt'>
-              <img
-                className='europa'
-                src={require('./images/logos-FEDR_.png')}
-                alt='europa'
-              />
-            </a>
+          <div className='image-text-row'>
+            <div className='image-text'>
+              <img src={require('./images/bySteel.png')} alt="bySteel" className="partner_imgs" />
+              <h4 className='company_name'>BYSTEEL FS S.A</h4>
+              <h4 className='company'>Company</h4>
+            </div>
+            <div className='image-text'>
+              <img src={require('./images/uMinho.png')} alt="uMinho" className="partner_imgs" />
+              <h4 className='company_name'>University of Minho</h4>
+              <h4 className='company'>Academia</h4>
+            </div>
+            <div className='image-text'>
+              <img src={require('./images/it.png')} alt="it" className="partner_imgs" />
+              <h4 className='company_name'>Instituto de Telecomunicações</h4>
+              <h4 className='company'>Academia</h4>
+            </div>
           </div>
-          <footer className='footer'>
-            <a
-              href='https://www.apache.org/licenses/LICENSE-2.0'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              Apache License
-            </a>
-            <h4>Version 2.0, January 2004</h4>
-            <h4 className='names'>Marina Albuquerque & Ricardo Mesquita</h4>
-          </footer>
         </div>
+        <div className='cofinanced'>
+          <h4>Co-financed by</h4>
+          <a href='https://www.compete2020.gov.pt/'>
+            <img className='compete2020' src={require('./images/level_compete.png')} alt='compete2020' />
+          </a>
+          <a href='https://portaldosincentivos.pt/index.php/portugal-2020'>
+            <img className='portugal2020' src={require('./images/level_portugal-2020.png')} alt='portugal2020' />
+          </a>
+          <a href='https://ec.europa.eu/regional_policy/funding/erdf_en?etrans=pt'>
+            <img className='europa' src={require('./images/logos-FEDR_.png')} alt='europa' />
+          </a>
+       
+
+        </div>
+        <footer className='footer'>
+          <a href='https://www.apache.org/licenses/LICENSE-2.0' target='_blank' rel='noopener noreferrer'>
+            Apache License
+          </a>
+          <h4>Version 2.0, January 2004</h4>
+          <h4 className='names'>Marina Albuquerque & Ricardo Mesquita</h4>
+        </footer>
+      </div>
     );
   }
+
   return null;
 }
 
